99018a982c4daa1a132db9b24a571008059ba272
estevansk8 <estevanmartins03@gmail.com>
2023-05-19 18:20:34 -0300

    :sparkles:feat: Function that see if the firsts 11 characters in a cpf follow the pattern of a real cpf

--- src/main/java/br/com/treinaweb/springbootAPI/entity/Pessoa.java
+++ src/main/java/br/com/treinaweb/springbootAPI/entity/Pessoa.java
@@ -49,9 +49,50 @@ public class Pessoa
     }
 
     public boolean validate_cpf(String cpf){
+        String ponto = ".";
+        String tracinho = "-";
         if (cpf.length() != 14) {
             return false;
         }
+        //reparte a string em partes verificando se cada uma possui os caracteres possiveis
+
+        //1 - verifica se os 3 primeiros caracteres são numeros
+        if (!cpf.substring(0,3).matches("[0-9]*")){
+            return false;
+        }
+        //2 - verifica se o primeiro ponto está no lugar correto
+        String ponto1 = String.valueOf(cpf.charAt(3));
+        if (!ponto1.equals(ponto)){
+            return false;
+        }
+
+        //3 - verifica se os 3 próximos caracteres são números
+        if (!cpf.substring(4,7).matches("[0-9]*")){
+            return false;
+        }
+
+        //4 - verifica se o segundo ponto está no lugar certo
+        String ponto2 = String.valueOf(cpf.charAt(7));
+        if (!ponto2.equals(ponto)){
+            return false;
+        }
+
+        //5 - verifica se os 3 proximos caracteres são numeros
+        if (!cpf.substring(8,11).matches("[0-9]*")){
+            return false;
+        }
+
+        //6 - verifica se o tracinho esta no lugar certo
+        String tracinhoIndx = String.valueOf(cpf.charAt(11));
+        if (!tracinhoIndx.equals(tracinho)){
+            return false;
+        }
+
+        //7 - verifica se os 2 proximos caracteres são numeros
+        if (!cpf.substring(12).matches("[0-9]*")){
+            return false;
+        }
+        //8 - verifica se os caracteres verificados no anterior seguem o padrao
         return true;
     }
 
