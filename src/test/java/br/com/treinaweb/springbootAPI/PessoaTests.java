f0cc2d8624450c9c61930d3e736a15a280f5a824
estevansk8 <estevanmartins03@gmail.com>
2023-05-26 18:19:44 -0300

    :sparkles:feat: More one test in PessoaTests that see if the person don't have animals is valid

--- src/test/java/br/com/treinaweb/springbootAPI/PessoaTests.java
+++ src/test/java/br/com/treinaweb/springbootAPI/PessoaTests.java
@@ -39,30 +39,28 @@ public class PessoaTests {
 
         //Criar lista de animais
         List<Animal> listaAnimal = new ArrayList<Animal>();
-
         //Eu- Criar pessoa para o animal
         Pessoa pessoa_dono = new Pessoa(1,"Miguelzinho","11144477735",listaAnimal);
 
         //Criar uns 2 ou 3 animais com preços diferente
         Animal animal1 = new Animal("Jacare", 20.00, pessoa_dono);
         listaAnimal.add(animal1);
-
         Animal animal2 = new Animal("Dragao", 45.50, pessoa_dono);
         listaAnimal.add(animal2);
-
         Animal animal3 = new Animal("Pangare", 10.50, pessoa_dono);
         listaAnimal.add(animal3);
 
         //Cria pessoa passando lista de animais
-
         //colocar no validador  o valor esperado(somando os preços) e chama a funcao para calcular
         //Pessoa pessoa = new Pessoa(1,"Miguelzinho", "111.444.777-35", listaAnimal);
-
         //melhor dos casos com valores corretos
         Assertions.assertEquals(76.00, pessoa_dono.calcular_gasto());
 
 
         //não adiciona os animais e passa a lista vazia
+        List<Animal> listaAnimal_vazia = new ArrayList<>();
+        Pessoa pessoaSemAnimais = new Pessoa(1,"Miguelzinho","11144477735",listaAnimal_vazia);
+        Assertions.assertEquals(0, pessoaSemAnimais.calcular_gasto());
 
         //passa um numero incorreto e ve o que acontece
 
